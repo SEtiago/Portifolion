a214b1287f1f0b97b2b3f7b8c6cd34de1ee71983
SEtiago <102744587+SEtiago@users.noreply.github.com>
2022-10-04 16:59:17 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+
+    $('#menu').click(function(){
+        $(this).toggleClass('fa-times');
+        $('header').toggleClass('toggle');
+    });
+
+    $(window).on('scroll load',function(){
+
+        $('#menu').removeClass('fa-times');
+        $('header').removeClass('toggle');
+
+        if($(window).scrolltop() > 0 ) {
+            $('.top').show();
+        }
+        else{
+            $('.top').hide();
+        }
+    });
+// smooth scrolling
+
+$('a[href *="#"]').on('click',function(e){
+    e.preventDefaut();
+    
+    $('html, body').animate({
+        scrolltop : $($(this).attr('href')).offset().top,
+    },
+    500,
+    'linear'
+    );
+});
+});
+
